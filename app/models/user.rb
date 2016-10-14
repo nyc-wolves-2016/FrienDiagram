b7a9843bf1f6e78964934d4e991d98c3d4fd4893
Edwin Carbajal & Neel Gupta & per ohrstrom & Thomas Graves <pair+edwincarbajal.neelgupta2.perohrstrom.thomaslogangraves@devbootcamp.com>
2016-10-13 20:25:44 -0400

    refactor associations for users and events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   has_many :friendships, foreign_key: :friender_id
-  has_many :friendees, through: :friendships, source: :user
-  has_many :frienders, through: :friendships, source: :user
+  has_many :friendees, through: :friendships, source: :friendee
+  has_many :frienders, through: :friendships, source: :friender
   has_many :user_addresses
+  has_many :invitations, foreign_key: :guest_id
+  has_many :events, foreign_key: :host_id
+  has_many :votes
+  has_many :voted_venues, through: :votes, source: :venue_choice
+
   has_secure_password
 
   def friends
