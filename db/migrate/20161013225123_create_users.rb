5c41c3efada690e24cd729736a8369764c86ada3
per ohrstrom <email": null>
2016-10-14 15:36:09 -0400

    redoing Devise commit

--- db/migrate/20161013225123_create_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |t|
-      t.string  :email, :password_digest, :username, null: false
-      t.timestamps
-    end
-  end
-end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
 
+<<<<<<< HEAD
+=======
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+>>>>>>> redoing Devise commit
 end
