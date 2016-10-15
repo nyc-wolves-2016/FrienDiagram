96320ce7a37b7d96a9bf27e419bacb11798c7ee4
Neel Gupta <neelgupta2@gmail.com>
2016-10-15 11:41:53 -0400

    Migrate state to App component from MapView.

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -5,14 +5,28 @@ class App extends React.Component {
       possibleVenues: [],
       midpoint: [],
       detailsView: {},
-      choices: []
+      lat: 40.705116,
+      lng: -74.00883,
+      choices: [
+        {
+          name: "freedomTower",
+          lat: 40.713230,
+          lng: -74.013367
+        },
+        {
+          name: "freedomTower",
+          lat: 40.759011,
+          lng: -73.9884472
+        }
+      ]
     }
   }
 
   render() {
+    let { choices, lat, lng, midpoint } = this.state
     return (
       <div className="app-container">
-        {/* <MapView /> */}
+        <MapView choices={choices} lat={lat} lng={lng} />
       </div>
     )
   }
