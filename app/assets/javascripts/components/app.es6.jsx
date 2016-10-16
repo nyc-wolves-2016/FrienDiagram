93d7be666be9caa298d73ced5e3f8edba9361d9b
Thomas Graves <email": null>
2016-10-16 19:01:19 -0400

    Add functions to pass data down to Venue component.

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -2,23 +2,14 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      possibleVenues: [
-        {
-          name: "freedomTower",
-          rating: 1,
-          price: "$$$",
-          lat: 40.713230,
-          lng: -74.013367
-        },
-        {
-          name: "freedomTower",
-          lat: 40.759011,
-          lng: -73.9884472
-        }
-      ],
+      possibleVenues: [],
+      midpoint: [40.705116, -74.00883],
+      selectedVenue: {},
+      // Will remove and use MidPoint as these variables
+      lat: 40.705116,
+      lng: -74.00883,
       venues: [],
       link: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCBINYOJFWMyIvjB33nka8JbXQ7ObqXaJA&libraries"
->>>>>>> Pass up GPlaces ping from MapView to App.
     }
     this.setEventDetails = this.setEventDetails.bind(this);
     this.grabPlaces = this.grabPlaces.bind(this);
@@ -39,13 +30,7 @@ class App extends React.Component {
   grabPlaces(venuesArray) {
     // Take places ping from MapView
     // Send info to ListView
-
-    this.setState((prevState) => {
-      return {
-        possibleVenues: [...prevState.possibleVenues, venuesArray]
-      }
-    })
-    debugger;
+    this.setState({ possibleVenues: venuesArray })
   }
 
   componentDidMount() {
@@ -57,17 +42,8 @@ class App extends React.Component {
     var types = "&types=food";
     var key = "&key=[KEY]";
     var radius = "&500";
-    // this.setState({possibleVenues: this.props.possibleVenues})
     //TODO This is where we want to ping Google
-    // var xhr = $.ajax({
-    //   url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"+location+rankby+types+radius+key,
-    //   type: "GET",
-    //   cache: false
-    // })
-    // // xhr.success(function(response) {
-    // debugger
-    //   this.setState({possibleVenues: response})
-    // })
+
   }
 
   componentWillMount() {
@@ -84,22 +60,23 @@ class App extends React.Component {
   }
 
   render() {
-    const { choices, lat, lng, midpoint, possibleVenues, detailsView } = this.state
+    const { lat, lng, midpoint, possibleVenues, detailsView } = this.state
     // Uncomment line below to see state change
-    console.log('This is my state', this.state)
+    // console.log('This is my state', this.state)
+    console.log(possibleVenues)
     return (
         <div className="app-container row">
             <div className="col-md-12">
               <MapView
               passUpPlaces={this.grabPlaces}
-              choices={choices}
+              venues={possibleVenues}
               lat={lat}
               lng={lng}
               mapLink={this.state.link} />
             </div>
 
             <div className="venue-list-container">
-              <VenueList handleData={this.setEventDetails} venues={ this.state.possibleVenues } />
+              <VenueList handleData={this.setEventDetails} venues={ possibleVenues } />
             </div>
             <div>
               <EventDetails venue={this.state.selectedVenue} details={this.props.event}/>
