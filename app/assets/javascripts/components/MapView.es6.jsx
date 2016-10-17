00450cd245fc4805ed7460289e3098bf3e0dfa09
Thomas Graves <email": null>
2016-10-17 14:40:55 -0400

    Finish code to add in markers for venues listener. Change midpoint color.

--- app/assets/javascripts/components/MapView.es6.jsx
+++ app/assets/javascripts/components/MapView.es6.jsx
@@ -4,7 +4,9 @@ class MapView extends React.Component {
     this.state = {
       possibleVenues: []
     }
+    this.addMarkerListener = this.addMarkerListener.bind(this)
   }
+
   componentDidMount() {
     const { searchType, lat, lng, midpoint, choices } = this.props
     //
@@ -12,29 +14,45 @@ class MapView extends React.Component {
     // this.initMap(lat,lng);
     this.map = new window.google.maps.Map(this.refs.map, {
       center: midpoint,
-      zoom: 15
+      zoom: 16
     });
     var midpointLocal = new google.maps.Marker({
       position: midpoint,
-      map: this.map
+      map: this.map,
+      icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
     });
     var request = {
       location: midpoint,
       radius: '500',
       types: searchType
     };
-    var service = new google.maps.places.PlacesService(this.map);
-    var results = service.nearbySearch(request, (results) => {
-        this.setState({possibleVenues: results})
-        this.props.passUpPlaces(this.state.possibleVenues)
-        for (var i = 0; i < results.length; i++) {
-          var placeLoc = results[i].geometry.location;
-          var marker = new google.maps.Marker({
-            map: this.map,
-            position: results[i].geometry.location
-          });
-        }
+    this.service = new google.maps.places.PlacesService(this.map);
+    var results = this.service.nearbySearch(request, (results) => {
+      this.setState({possibleVenues: results})
+      this.props.passUpPlaces(this.state.possibleVenues)
+      for (var i = 0; i < results.length; i++) {
+        var placeLoc = results[i].geometry.location;
+        var marker = new google.maps.Marker({
+          map: this.map,
+          position: results[i].geometry.location
+        });
+        this.addMarkerListener(marker, results[i])
+      }
     });
+  }
+
+  addMarkerListener(marker, data) {
+    google.maps.event.addListener(marker, 'click', function() {
+        this.service.getDetails(data, (results, status) => {
+        if (status !== google.maps.places.PlacesServiceStatus.OK) {
+          console.error(status);
+          return;
+        }
+        var infoWindow = new google.maps.InfoWindow();
+        infoWindow.setContent(data.name);
+        infoWindow.open(this.map, marker);
+      });
+    }.bind(this));
   }
 
   render() {
