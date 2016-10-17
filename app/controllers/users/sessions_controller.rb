3302f09c4dedf591eaa525dd02e0de4215fa8223
Edwin Carbajal <email": null>
2016-10-17 10:58:53 -0400

    fix issue

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,4 @@
-class User::SessionsController < Devise::SessionsController
+class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
