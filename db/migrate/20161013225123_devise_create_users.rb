1d3887c528a1cb62402ee14ce2227c1405bc5f55
Edwin Carbajal <email": null>
2016-10-18 12:04:44 -0400

    materialized sign up form

--- db/migrate/20161013225123_devise_create_users.rb
+++ db/migrate/20161013225123_devise_create_users.rb
@@ -2,9 +2,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.string :username, null: false, default: ""
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
+      t.string :username, null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -60,19 +60,21 @@ ActiveRecord::Schema.define(version: 20161013233056) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "username",               default: "", null: false
+    t.string   "first_name",                         null: false
+    t.string   "last_name",                          null: false
+    t.string   "email",                              null: false
+    t.string   "encrypted_password",                 null: false
+    t.string   "username",                           null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
