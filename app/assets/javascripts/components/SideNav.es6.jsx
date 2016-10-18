f7541d28d9499d511f51ea66736f2722d8cb0d62
per ohrstrom <email": null>
2016-10-18 10:21:03 -0400

    refactor 'bind' in handleToggle of the Nav component

--- app/assets/javascripts/components/SideNav.es6.jsx
+++ app/assets/javascripts/components/SideNav.es6.jsx
@@ -4,14 +4,14 @@ class SideNav extends React.Component {
     $(".button-collapse").sideNav({
       menuWidth: 300, // Default is 240
       edge: 'left', // Choose the horizontal origin
-    });
+    }.bind(this));
     // Initialize collapsible (uncomment the line below if you use the dropdown variation)
     //$('.collapsible').collapsible();
   }
 
   render() {
     return(
-      <a href="#" onClick={this.handleToggle.bind(this)} data-activates="slide-out" className="button-collapse">
+      <a href="#" onClick={this.handleToggle} data-activates="slide-out" className="button-collapse">
         <i className="material-icons">menu</i>
       </a>
     );
