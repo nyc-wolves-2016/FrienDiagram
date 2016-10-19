482b5f70e55bb92611d867a4daa8063df1c536d0
Neel Gupta & Thomas Graves <pair+neelgupta2.thomaslogangraves@devbootcamp.com>
2016-10-18 21:01:36 -0400

    Add in controller route for creating venueChoices.

--- app/controllers/venue_choices_controller.rb
+++ app/controllers/venue_choices_controller.rb
@@ -1,10 +1,16 @@
 class VenueChoicesController < ApplicationController
 
   def create
-    event = Event.find_by(id: params[:event_id])
-    venue = VenueChoice.create(venue_params)
-    event.venue_choices << venue
-    Vote.create(user: current_user, venue_choice: venue )
+    event = Event.find(params[:event_id])
+    binding.pry
+    venue = VenueChoice.new(venue_params)
+    if venue.save
+      binding.pry
+    end
+  end
+
+  def delete
+    binding.pry
   end
 
   def venue_params
